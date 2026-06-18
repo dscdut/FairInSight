031b6f833427129bc9dbf4509c7269ef6917e6e7
Nguyen Hong Son <snguyenhong8@gmail.com>
2026-06-18 14:08:09 +0700

    fix(FI-59): remove footer and header in landing page, fix responsive padding

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,15 +1,12 @@
-import Header from '@/components/header-nav/header-nav'
 import { AboutSection } from '@/pages/home/components/about-section'
 import { AISection } from '@/pages/home/components/ai-section'
 import { ContactSection } from '@/pages/home/components/contact-section'
-import { FooterSection } from '@/pages/home/components/footer-section'
 import { HeroSection } from '@/pages/home/components/hero-section'
 import { LibrarySection } from '@/pages/home/components/library-section'
 
 const HomePage = () => {
   return (
     <div className='min-h-screen bg-white dark:bg-gray-900'>
-      <Header />
       <main>
         <HeroSection />
         <AboutSection />
@@ -17,8 +14,6 @@ const HomePage = () => {
         <AISection />
         <ContactSection />
       </main>
-
-      <FooterSection />
     </div>
   )
 }
