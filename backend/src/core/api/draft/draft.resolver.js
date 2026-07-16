5fffc161b9276a40c124619d2373e2ed71476201
hoanninh123 <anninh2508@gmail.com>
2026-07-16 15:58:54 +0700

    add swagger descriptions for auto-save draft feature

--- backend/src/core/api/draft/draft.resolver.js
+++ backend/src/core/api/draft/draft.resolver.js
@@ -14,12 +14,14 @@ export const DraftResolver = Module.builder()
         {
             route: '/',
             method: 'get',
+            description: 'Get list of user drafts',
             controller: DraftController.listDrafts,
             preAuthorization: true
         },
         {
             route: '/',
             method: 'post',
+            description: 'Create a new draft',
             interceptors: [CreateDraftInterceptor],
             body: 'CreateDraftDto',
             controller: DraftController.createDraft,
@@ -28,6 +30,7 @@ export const DraftResolver = Module.builder()
         {
             route: '/:id',
             method: 'put',
+            description: 'Auto save a draft (Update)',
             params: [RecordId],
             interceptors: [RecordIdInterceptor, UpdateDraftInterceptor],
             body: 'UpdateDraftDto',
@@ -37,6 +40,7 @@ export const DraftResolver = Module.builder()
         {
             route: '/:id',
             method: 'delete',
+            description: 'Delete a draft',
             params: [RecordId],
             interceptors: [RecordIdInterceptor],
             controller: DraftController.deleteDraft,
